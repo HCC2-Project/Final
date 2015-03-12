77a5bc74fa9cff205b1b272e8c18ac382a99c53e
x13728111 <x13728111@scr1-007-PC.student.nci.col>
2015-03-12 17:32:07 +0000

    Leaderboard

--- index.php
+++ index.php
@@ -10,21 +10,21 @@
             <!-- Three columns of text below the carousel -->
             <div class="row">
                 <div class="col-lg-4">
-                    <img class="img-circle" src="http://www.benryves.com/images/bletchley_park/2010.05.08/23-WITCH.jpg" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+                    <img class="img-circle" src="images(1).jpg" alt="images(1).jpg" style="width: 140px; height: 140px;">
                     <h3>First Generation</h3>
                     <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
                     <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
                 </div><!-- /.col-lg-4 -->
 
                 <div class="col-lg-4">
-                    <img class="img-circle" src="http://www.techiwarehouse.com/userfiles/ic-chip-example.jpg" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+                    <img class="img-circle" src="images(1).jpg" alt="images(1).jpg" style="width: 140px; height: 140px;">
                     <h3>Second Generation</h3>
                     <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
                     <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
                 </div><!-- /.col-lg-4 -->
 
                 <div class="col-lg-4">
-                    <img class="img-circle" src="http://i.ytimg.com/vi/gHm5_74nP38/hqdefault.jpg" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+                    <img class="img-circle" src="images(1).jpg" alt="images(1).jpg" style="width: 140px; height: 140px;">
                     <h3>Third Generation</h3>
                     <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
                     <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
@@ -45,7 +45,7 @@
                     <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
                 </div>
                 <div class="col-md-5">
-                    <img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="images (1).jpg">
+                    <img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="images(1).jpg">
                 </div>
             </div>
 
@@ -53,7 +53,7 @@
 
             <div class="row featurette">
                 <div class="col-md-5">
-                    <img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="images (1).jpg">
+                    <img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="images(1).jpg">
                 </div>
                 <div class="col-md-7">
                     <h2 class="featurette-heading">Oh yeah, its that good. <span class="text-muted">See for yourself.</span></h2>
@@ -69,7 +69,7 @@
                     <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
                 </div>
                 <div class="col-md-5">
-                    <img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="images (1).jpg">
+                    <img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="images(1).jpg">
                 </div>
             </div>
 
