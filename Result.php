9f2c1fe63f57a1fa3fdd79e9529f4e9956b0ae71
X13508827 <X13508827@scr1-004.student.nci.col>
2015-03-12 15:44:30 +0000

    SenaOn

---
+++ Result.php
@@ -0,0 +1,20 @@
+<?php//quiz score
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//https://youtu.be/DoTGt9CnC5Y code source
+mysql_connect("connect to the database", "username", "password")or die("connection fail");
+mysql_select_db("database name")or die("connection fail");
+$name = $-POST['username'];
+$query ="select* from results where username = --";
+$results = mysql-query($query);
+while($line = mysql_fetch_array($results, MYSQL_ASSOC)){
+    echo "$line['username']."<br/>;
+    echo "$line['subject1']." </div> </td>"." <br />;
+        
+}
+
+?>
