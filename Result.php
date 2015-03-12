7422cc9f60a0a10b5fa57125f7978031e50959a1
kevgleeson78 <kevgleeson78@gmail.com>
2015-03-12 16:23:52 +0000

    Merge origin/master
    Conflicts:
    	login.php
    	nbproject/project.properties
    	nbproject/project.xml

--- Result.php
+++ Result.php
@@ -7,14 +7,13 @@
  */
 //https://youtu.be/DoTGt9CnC5Y code source
 //connect to the databse
-mysql_connect("localhost", "username", " ")or die("connection fail");
-mysql_select_db("database name")or die("connection fail");
+mysql_connect("localhost", "root", "password")or die("connection fail");
+mysql_select_db("result")or die("connection fail");
 $name = $-POST['username'];
 $query ="select* from results where username = --";
 $results = mysql-query($query);
 while($line = mysql_fetch_array($results, MYSQL_ASSOC)){
     echo "$line['username']."<br/>;
-    echo "$line['subject1']." </div> </td>"." <br />;
         
 }
 
