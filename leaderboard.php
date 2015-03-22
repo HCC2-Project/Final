bcf04c905913ff1ee44400f1da0aea3811637b44
kevgleeson78 <kevgleeson78@gmail.com>
2015-03-22 23:52:59 +0000

    I have the quiz score writing to the database, its a bit basic at the moment but i will do some more work on it over the next week. Also i have the top ten scores printed in a list on the leaderboard page.

--- leaderboard.php
+++ leaderboard.php
@@ -1,68 +1,23 @@
 <?php
- require_once 'header.php';
-
-  echo "<br><span class='main'>Welcome to $appname,";
-
-  if ($loggedin) echo " $user, you are logged in.";
-  else           echo ' please sign up and/or log in to join in.';
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-      echo<<<_END
-  ' <div class="container marketing">
-
-
-
-            <!-- START THE FEATURETTES -->
-
-           
-<head>
-<title>Leaderboard</title>
-<link rel ="stylesheet" type="text/css" href="carousel.css"/>
-<meta http-equiv="Content-Type" content="leaderboard/php;charset=utf-8" />
-</head>
-<body>
-<div id="Wrapper">
-<div id="Header"><h1>WELCOME TO THE LEADERBOARD</h1></div>
-	<form method="post" action="results1.php">
-		<input type="text" name="name" value="Name" /><br \>
-		<input type="text" name="score" value="Score" /><br \> <br \> <br \> <br \> 
-		<input type="submit" value="Submit Score to Leaderboards" />
-	</form>
-	
-	<div id ="sidebarR">
-
-<h2>LEADERBOARD RESULTS</h2>
-<input type="text"name="place" value="1."/>
-<input type="text"name="place" value="2."/>
-<input type="text"name="place" value="3."/>
-<input type="text"name="place" value="4."/>
-<input type="text"name="place" value="5."/>
-<input type="text"name="place" value="6."/>
-<input type="text"name="place" value="7."/>
-<input type="text"name="place" value="8."/>
-<input type="text"name="place" value="9."/>
-<input type="text"name="place" value="10."/>
-</div>
-</div>	
-
-
-            <!-- /END THE FEATURETTES -->
-
-
-            <!-- FOOTER -->
-            <footer>
+require_once 'header.php';
+    // Send variables for the MySQL database class.
+    $database = mysql_connect('localhost', 'root', 'password') or die('Could not connect: ' . mysql_error());
+    mysql_select_db('robinsnest') or die('Could not select database');
+ 
+    $query = "SELECT * FROM `members` ORDER by `quiz_score` DESC LIMIT 10";
+    $result = mysql_query($query) or die('Query failed: ' . mysql_error());
+ 
+    $num_results = mysql_num_rows($result);  
+ 
+    for($i = 0; $i < $num_results; $i++)
+    {
+         $row = mysql_fetch_array($result);
+         echo $row['user'] . "\t" . $row['quiz_score'] . "<br/>";
+    }
+    echo '<footer>
                 <p class="pull-right"><a href="#">Back to top</a></p>
                 <p>&copy; 2014 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
-            </footer>
-
-        </div><!-- /.container -->'
-            
-_END;
+            </footer>';
 ?>
-    </span><br><br>
-  </body>
+</body>
 </html>
