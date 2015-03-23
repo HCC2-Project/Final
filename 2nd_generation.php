a5ce4d1bdc0820118f3ec8034ae672a6b1f30019
SenaOn <>
2015-03-23 13:18:16 +0000

    Added contents and images

--- 2nd_generation.php
+++ 2nd_generation.php
@@ -6,7 +6,33 @@
  * and open the template in the editor.
  */
   require_once 'header.php';
+echo '<h2> Second Generation (1956-1963) Transistors</h2>
+ <div class ="paragraph">    
+ <p>
 
+Transistors replaced vacuum tubes and ushered in the second generation of computers. 
+The transistor was invented in 1947 but did not see widespread use in computers until the late 1950s. 
+The transistor was far superior to the vacuum tube, allowing computers to become smaller, faster, cheaper, 
+more energy-efficient and more reliable than their first-generation predecessors. Though the transistor still generated a 
+great deal of heat that subjected the computer to damage, it was a vast improvement over the vacuum tube. Second-generation 
+computers still relied on punched cards for input and printouts for output.<br/> <br/>
+Second-generation computers moved from cryptic binary machine language to symbolic, or assembly, languages, which allowed programmers to specify instructions in words. High-level programming languages were also being developed at this time, such as early versions of COBOL and FORTRAN. 
+These were also the first computers that stored their instructions in their memory, which moved from a magnetic drum to magnetic core technology.
+<div class ="link"> <a href="http://www.webopedia.com/DidYouKnow/Hardware_Software/FiveGenerations.asp">Read more</a></div> 
+ </p>
+   
+   <h2> Image of Second Computer</h2>
+ <div class ="fcomputer"><img src="Images/transistor-computer.jpg" style="width: 600px; height: 400px;"></div>
+    
+<br/> <br/>
+<!-- FOOTER -->
+            <footer>
+                <p class="pull-right"><a href="#">Back to top</a></p>
+                <p>&copy; 2014 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+            </footer>
+
+</div>'
+  
    ?>
 
     </span><br><br>
