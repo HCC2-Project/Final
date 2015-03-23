6a3d18d3ed6dfe9ef3e8404383e29fadc2172452
SenaOn <>
2015-03-23 13:18:50 +0000

    Added contents and images

--- 4th_generation.php
+++ 4th_generation.php
@@ -7,7 +7,28 @@
  */
 
 require_once 'header.php';
+echo '<h2> Fourth Generation (1971-Present) Microprocessors</h2>
+ <div class ="paragraph">    
+ <p>
 
+The microprocessor brought the fourth generation of computers, as thousands of integrated circuits were built onto a single silicon chip. What in the first generation filled an entire room could now fit in the palm of the hand. The Intel 4004 chip, developed in 1971,
+located all the components of the computer—from the central processing unit and memory to input/output controls—on a single chip..<br/> <br/>
+
+In 1981 IBM introduced its first computer for the home user, and in 1984 Apple introduced the Macintosh. Microprocessors also moved out of the realm of desktop computers and into many areas of life as more and more everyday products began to use microprocessors.
+<div class ="link"> <a href="http://www.webopedia.com/DidYouKnow/Hardware_Software/FiveGenerations.asp">Read more</a></div> 
+ </p>
+   
+   <h2> Image of Fourth Computer</h2>
+ <div class ="fcomputer"><img src="Images/microchips.jpg" style="width: 600px; height: 400px;"></div>
+   
+<br/> <br/>
+<!-- FOOTER -->
+            <footer>
+                <p class="pull-right"><a href="#">Back to top</a></p>
+                <p>&copy; 2014 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+            </footer>
+
+</div>'
  ?>
 
     </span><br><br>
