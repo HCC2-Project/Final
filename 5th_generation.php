c7f5d2adde5b18b12e8d11bc283082bccba37660
SenaOn <>
2015-03-23 13:19:05 +0000

    Added contents and images

--- 5th_generation.php
+++ 5th_generation.php
@@ -7,7 +7,27 @@
  */
 
 require_once 'header.php';
+echo '<h2> Fifth Generation (Present and Beyond) Artificial Intelligence</h2>
+ <div class ="paragraph">    
+ <p>
 
+Fifth generation computing devices, based on artificial intelligence, are still in development, though there are some applications, such as voice recognition, that are being used today. 
+The use of parallel processing and superconductors is helping to make artificial intelligence a reality. Quantum computation and molecular and nanotechnology will radically change the face of computers in years to come. 
+The goal of fifth-generation computing is to develop devices that respond to natural language input and are capable of learning and self-organization.
+<div class ="link"> <a href="http://www.webopedia.com/DidYouKnow/Hardware_Software/FiveGenerations.asp">Read more</a></div> 
+ </p>
+   
+   <h2> Image of Fifth Computer</h2>
+ <div class ="fcomputer"><img src="Images/Artificial Intelligence.jpg" style="width: 600px; height: 400px;"></div>
+    
+<br/> <br/>
+<!-- FOOTER -->
+            <footer>
+                <p class="pull-right"><a href="#">Back to top</a></p>
+                <p>&copy; 2014 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+            </footer>
+
+</div>'
  ?>
 
     </span><br><br>
