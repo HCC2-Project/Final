1a7e894b3062fdf28be1935be6f48ffe21b82353
User <User@192.168.222.42>
2015-03-23 11:07:54 +0000

    Merge origin/master

--- JS/quizconfig.js
+++ JS/quizconfig.js
@@ -43,26 +43,26 @@ window.location="results.htm"
 
 
 function showsolution(){
-var win2=window.open("","win2","width=200,height=350, scrollbars")
-win2.focus()
-win2.document.open()
-win2.document.write('<title>Solution</title>')
-win2.document.write('<body bgcolor="#FFFFCC">')
-win2.document.write('<center><h3>Solution to Quiz</h3></center>')
-win2.document.write('<center><font face="Arial">')
+var results=window.open("","results","width=200,height=350, scrollbars")
+results.focus()
+results.document.open()
+results.document.write('<title>Solution</title>')
+results.document.write('<body bgcolor="#FFFFCC">')
+results.document.write('<center><h3>Solution to Quiz</h3></center>')
+results.document.write('<center><font face="Arial">')
 for (i=1;i<=totalquestions;i++){
 for (temp=0;temp<incorrect.length;temp++){
 if (i==incorrect[temp])
 wrong=1
 }
 if (wrong==1){
-win2.document.write("Question "+i+"="+correctchoices[i].fontcolor("red")+"<br>")
+results.document.write("Question "+i+"="+correctchoices[i].fontcolor("red")+"<br>")
 wrong=0
 }
 else
-win2.document.write("Question "+i+"="+correctchoices[i].fontcolor("green")+"<br>")
+results.document.write("Question "+i+"="+correctchoices[i].fontcolor("green")+"<br>")
 }
-win2.document.write('</center></font>')
-win2.document.write("<h5>Note:The solutions in red are the ones to the questions you had incorrectly answered.</h5><p align='center'><small>")
-win2.document.close()
+results.document.write('</center></font>')
+results.document.write("<h5>Note:The solutions in red are the ones to the questions you had incorrectly answered.</h5><p align='center'><small>")
+results.document.close()
 }
