dc9fbd3e42a655555112d6afaaa412f45b4ef2ba
SenaOn <>
2015-03-23 13:17:57 +0000

    Added contents and images

--- 1st_generation.php
+++ 1st_generation.php
@@ -7,6 +7,27 @@
  */
   require_once 'header.php';
 
+ echo '<h2> First Generation (1940-1956) Vacuum Tubes</h2>
+ <div class ="paragraph">    
+ <p>
+  The first computers used vacuum tubes for circuitry and magnetic drums for memory, and were often enormous, taking up entire rooms. 
+  They were very expensive to operate and in addition to using a great deal of electricity, generated a lot of heat, which was often the cause of malfunctions.
+  First generation computers relied on machine language, the lowest-level programming language understood by computers, to perform operations, and they could only solve one problem at a time. 
+  Input was based on punched cards and paper tape, and output was displayed on printouts. The UNIVAC and ENIAC computers are examples of first-generation computing devices. 
+  The UNIVAC was the first commercial computer delivered to a business client, the U.S. Census Bureau in 1951.
+   <div class ="link"> <a href="http://www.webopedia.com/DidYouKnow/Hardware_Software/FiveGenerations.asp">Read more</a></div> 
+ </p>
+   
+   <h2> Image of First Computer</h2>
+ <div class ="fcomputer"><img src="Images/UNIVAC_computer.jpg" style="width: 700px; height: 400px;"></div>
+   
+<br/> <br/>
+<!-- FOOTER -->
+            <footer>
+                <p class="pull-right"><a href="#">Back to top</a></p>
+                <p>&copy; 2014 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+            </footer>
+</div>'
   
   ?>
 
