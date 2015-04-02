b19a3cb28d1b253b06cd6f066424381ac2e09b81
x13728111 <x13728111@SCR1-0006.student.nci.col>
2015-04-02 16:35:21 +0100

    Maricel: Content for Question 1.

--- 1st_generation.php
+++ 1st_generation.php
@@ -10,6 +10,7 @@
  echo '<h2> First Generation (1940-1956) Vacuum Tubes</h2>
  <div class ="paragraph">    
  <p>
+ Content for Question 1.
  Introduction <br/>
  
  The calculating engines of English mathematician Charles Babbage (1791-1871) are among the most celebrated icons in 
@@ -17,6 +18,7 @@
  remains one of the finest examples of precision engineering of the time. Babbage is sometimes referred to as 
  "father of computing." The International Charles Babbage Society (later the Charles Babbage Institute) took his name 
  to honor his intellectual contributions and their relation to modern computers.
+ 
  Biographical Note <br/>
  
  Charles Babbage was born in London on December 26, 1791, the son of Benjamin Babbage, a London banker. As a youth 
@@ -38,7 +40,33 @@
  computers are examples of first-generation computing devices. 
  The UNIVAC was the first commercial computer delivered to a business client, the U.S. Census Bureau in 1951.
  <div class ="link"> <a href="http://www.webopedia.com/DidYouKnow/Hardware_Software/FiveGenerations.asp">Read more</a></div> 
- </p>
+
+Content for Question3.
+Joseph-Marie Jacquard was not the inventor of the programmable loom, as many people imagine, actually he created an 
+attachment to the loom, which played a very important role not only in the textile industry, but also in development 
+of other programmable machines, such as computers, for example the Analytical Engine of Charles Babbage.
+
+Joseph-Marie Charles (known as Jacquard after the nickname of his family) was born on 7th of July, 1752, in Lyon, 
+France. He was the fifth of nine children of Jean Charles (nicknamed Jacquard) (1724-1772), a master weaver of 
+brocaded fabrics, and his wife, Antoinette Rive. From the nine children, only Joseph and his sister Clémence 
+(born 1747) survived to adulthood. Like the sons of many Lyons weavers, Joseph did not go to school, because his 
+father needed him to perform odd jobs in the workshop. He learned to read as a thirteen-year-old boy by his 
+brother-in-law Barret, a cultured man.
+
+One of the first improvements of Jacquard was to eliminate the paper strip from Vaucanson s mechanism and to return 
+to Falcons chain of punched cards. Then, he tried to avoid the expensive metal cylinders of Vaucanson. In fact, the.
+term Jacquard loom is a misnomer, actually Jacquard s invented an attachment (head), that adapts to a great many type 
+of looms, that allow the weaving machine to create the intricate patterns often seen in Jacquard weaving. Thus any 
+loom that uses the attachment is called a Jacquard loom.
+
+Each position in the punched card of the loom corresponds to a hook, which can either be raised or stopped dependant 
+on whether the hole is punched out of the card or the card is solid. The hook raises or lowers the harness, which 
+carries and guides the warp thread so that the weft will either lie above or below it. The sequence of raised and 
+lowered threads is what creates the pattern. Each hook can be connected via the harness to a number of 
+threads, allowing more than one repeat of a pattern. For example, a loom with a 500-hook head might have 
+four threads connected to each hook, resulting in a fabric that is 2000 warp ends wide with four repeats of 
+the weave going across.
+</p>
    
    <h2> Image of First Computer</h2>
  <div class ="fcomputer"><img src="Images/UNIVAC_computer.jpg" style="width: 700px; height: 400px;"></div>
--- 4th_generation.php
+++ 4th_generation.php
@@ -10,11 +10,15 @@ require_once 'header.php';
 echo '<h2> Fourth Generation (1971-Present) Microprocessors</h2>
  <div class ="paragraph">    
  <p>
+Content for Question 4.
+The microprocessor brought the fourth generation of computers, as thousands of integrated circuits were built onto a 
+single silicon chip. What in the first generation filled an entire room could now fit in the palm of the hand. The 
+Intel 4004 chip, developed in 1971,located all the components of the computer—from the central processing unit and 
+memory to input/output controls—on a single chip..<br/> <br/>
 
-The microprocessor brought the fourth generation of computers, as thousands of integrated circuits were built onto a single silicon chip. What in the first generation filled an entire room could now fit in the palm of the hand. The Intel 4004 chip, developed in 1971,
-located all the components of the computer—from the central processing unit and memory to input/output controls—on a single chip..<br/> <br/>
-
-In 1981 IBM introduced its first computer for the home user, and in 1984 Apple introduced the Macintosh. Microprocessors also moved out of the realm of desktop computers and into many areas of life as more and more everyday products began to use microprocessors.
+In 1981 IBM introduced its first computer for the home user, and in 1984 Apple introduced the Macintosh. 
+Microprocessors also moved out of the realm of desktop computers and into many areas of life as more and more 
+everyday products began to use microprocessors.
 <div class ="link"> <a href="http://www.webopedia.com/DidYouKnow/Hardware_Software/FiveGenerations.asp">Read more</a></div> 
  </p>
    
