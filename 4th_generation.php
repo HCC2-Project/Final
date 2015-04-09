6d3b036ae198662a234533036c534c177fc9f264
x13728111 <x13728111@SCR1-0006.student.nci.col>
2015-04-09 15:55:33 +0100

    Maricel: Content.

--- 4th_generation.php
+++ 4th_generation.php
@@ -10,6 +10,48 @@ require_once 'header.php';
 echo '<h2> Fourth Generation (1971-Present) Microprocessors</h2>
  <div class ="paragraph">    
  <p>
+ The basis of the fourth generation was the invention of the microprocessor by a team at Intel.
+Microprocessor-based computers were originally very limited in their computational ability and speed, and were in no 
+way an attempt to downsize the minicomputer. They were addressing an entirely different market.
+Although processing power and storage capacities have grown beyond all recognition since the 1970s, the underlying 
+technology of large-scale integration (LSI) or very-large-scale integration (VLSI) microchips has remained basically 
+the same, so it is widely regarded that most of today s computers still belong to the fourth generation.
+On November 15, 1971, Intel released the world s first commercial microprocessor, the 4004. It was developed for a 
+Japanese calculator company, Busicom, as an alternative to hardwired circuitry, but computers were developed 
+around it, with much of their processing abilities provided by one small microprocessor chip. Coupled with 
+one of Intel s other products - the RAM chip, based on an invention by Robert Dennard of IBM, 
+(kilobits of memory on one chip) - the microprocessor allowed fourth generation computers to be smaller 
+and faster than prior computers. The 4004 was only capable of 60,000 instructions per second, but its 
+successors, the Intel 8008, 8080 (used in many computers using the CP/M operating system), and the 8086/8088 family 
+(the IBM personal computer (PC) and compatibles use processors still backwards-compatible with the 8086)
+brought ever-growing speed and power to the computers. Other producers also made microprocessors which were 
+widely used in microcomputers.
+Mainframes and minicomputers
+Before the introduction of the microprocessor in the early 1970s, computers were generally large, costly systems 
+owned by large institutions: corporations, universities, government agencies, and the like. Users—who were 
+experienced specialists—did not usually interact with the machine itself, but instead prepared tasks for the 
+computer on off-line equipment, such as card punches. A number of assignments for the computer would be gathered up 
+and processed in batch mode. After the jobs had completed, users could collect the output printouts and punched cards.
+In some organizations it could take hours or days between submitting a job to the computing center and receiving the 
+output.
+
+A more interactive form of computer use developed commercially by the middle 1960s. In a time-sharing system, 
+multiple teleprinter terminals let many people share the use of one mainframe computer processor. This was common in 
+business applications and in science and engineering.
+
+A different model of computer use was foreshadowed by the way in which early, pre-commercial, experimental computers 
+were used, where one user had exclusive use of a processor.[7] Some of the first computers that might be called
+personal were early minicomputers such as the LINC and PDP-8, and later on VAX and larger minicomputers from Digital 
+Equipment Corporation (DEC), Data General, Prime Computer, and others. They originated as peripheral processors for 
+mainframe computers, taking on some routine tasks and freeing the processor for computation. By today s standards 
+they were physically large (about the size of a refrigerator) and costly (typically tens of thousands of US dollars),
+
+and thus were rarely purchased by individuals. However, they were much smaller, less expensive, and generally simpler
+to operate than the mainframe computers of the time, and thus affordable by individual laboratories and research 
+projects. Minicomputers largely freed these organizations from the batch processing and bureaucracy of a commercial 
+or university computing center.
+
+In addition, minicomputers were more interactive than mainframes, and soon had their own operating systems. The minicomputer Xerox Alto (1973) was a landmark step in the development of personal computers, because of its graphical user interface, bit-mapped high resolution screen, large internal and external memory storage, mouse, and special software.[8]
 Content for Question 4.
 The microprocessor brought the fourth generation of computers, as thousands of integrated circuits were built onto a 
 single silicon chip. What in the first generation filled an entire room could now fit in the palm of the hand. The 
