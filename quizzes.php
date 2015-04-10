b7c4345fd62d6ef62e06914a35350de2cb68573d
x13508827 <x13508827@scr1-004.student.nci.col>
2015-04-10 12:34:01 +0100

    Added 15 questions from each generations

--- quizzes.php
+++ quizzes.php
@@ -24,10 +24,9 @@ font-size: 16px;
 
 <form method="POST" name="myquiz">
 
+<hr class="featurette-divider">
 
 <div class="qheader">
-
-<hr class="featurette-divider">
 1)  Who was known as the Father of Computers?</div>
 <div class="qselections">
 <input type="radio" value="a" name="question1"> a)Blasie Pascal<br>
@@ -133,6 +132,171 @@ font-size: 16px;
 <input type = "radio" value = "b" name = "question10"> b) ENIAC<br>
 <input type = "radio" value = "c" name = "question10"> c) Apple lle<br>
 <input type = "radio" value = "d" name = "question10"> d) none of the above<br>
+</div>
+
+<div class="qheader">
+
+<hr class="featurette-divider">
+11)  Who was known as the Father of Computers?</div>
+<div class="qselections">
+<input type="radio" value="a" name="question11"> a)Blasie Pascal<br>
+<input type="radio" value="b" name="question11"> b)Joseph Jacquard<br>
+<input type="radio" value="c" name="question11"> c)Charles Babbage<br>
+<input type="radio" value="d" name="question11"> d)Herman Hollerith<br>
+</div>
+
+<hr class="featurette-quizhr">
+
+<div class="qheader">
+12)The Second Generation of computers used this to control operations:</div>
+<div class="qselections">
+<input type="radio" value="a" name="question12"> a)transistors<br>
+<input type="radio" value="b" name="question12"> b)microchips<br>
+<input type="radio" value="c" name="question12"> c)integrated circuits<br>
+<input type="radio" value="d" name="question12"> d)vacuum tubes<br>
+</div>
+
+<hr class="featurette-quizhr">
+
+<div class="qheader">
+13)Joseph Jacquard used to control he operations of his loom?</div>
+<div class="qselections">
+<input type="radio" value="a" name="question13"> a) Vacuum tubes<br>
+<input type="radio" value="b" name="question13"> b) electricity<br>
+<input type="radio" value="c" name="question13"> c) transistors<br>
+<input type="radio" value="d" name="question13"> d) punched cards<br>
+</div>
+
+<hr class="featurette-quizhr">
+
+<div class="qheader">
+14) The Fourth Generation of computers used this to control operations:</div>
+<div class="qselections">
+<input type="radio" value="a" name="question14"> a) integrated circuits<br>
+<input type="radio" value="b" name="question14"> b) transistors<br>
+<input type="radio" value="c" name="question14"> c) microchips<br>
+<input type="radio" value="d" name="question14"> d) vacuum tubes<br>
+</div>
+
+<hr class="featurette-quizhr">
+
+<div class="qheader">
+15) The arithmetic/logic unit of the computer:</div>
+<div class="qselections">
+<input type="radio" value="a" name="question15"> a) can add numbers<br>
+<input type="radio" value="b" name="question15"> b) can compare numbers<br>
+<input type="radio" value="c" name="question15"> c) is part of the central processing unit<br>
+<input type="radio" value="d" name="question15"> d) all of the above<br>
+</div>
+
+<hr class="featurette-quizhr">
+
+<div class="qheader">
+16) Which of the following is the correct order of the processing cycle of a computer?</div>
+<div class="qselections">
+<input type="radio" value="a" name="question16"> a) processing,, input, memory, output<br>
+<input type="radio" value="b" name="question16"> b) output, input, memory, processing<br>
+<input type="radio" value="c" name="question16"> c) input, memory , processing, output<br>
+<input type="radio" value="d" name="question16"> d) memory, input, output, processing<br>
+</div>
+
+<hr class="featurette-quizhr">
+ 
+<div class="qheader">
+17) What device is BOTH an input and output device?</div>
+<div class="qselections">
+<input type="radio" value="a" name="question17"> a) printer<br>
+<input type="radio" value="b" name="question17"> b) disk<br>
+<input type="radio" value="c" name="question17"> c) monitor<br>
+<input type="radio" value="d" name="question17"> d) keyboard<br>
+</div>
+
+<hr class="featurette-quizhr">
+
+<div class="qheader">
+18)Which software firm is the second largest in the world, after Microsoft?</div>
+<div class="qselections">
+<input type="radio" value="a" name="question18"> a) Netscape<br>
+<input type="radio" value="b" name="question18"> b) Oracle<br>
+<input type="radio" value="c" name="question18"> c) Lotus<br>
+<input type="radio" value="d" name="question18"> d) Norton<br>
+</div>
+
+<hr class="featurette-quizhr">
+
+<div class="qheader">
+19) As of January 2003, how much is Microsoft Chairman Bill Gates\'s net worth?</div>
+<div class = "qselections">
+<input type = "radio" value = "a" name = "question19"> a) 10 million US<br>
+<input type = "radio" value = "b" name = "question19"> b) 20 billion US<br>
+<input type = "radio" value = "c" name = "question19"> c) 35 billion US<br>
+<input type = "radio" value = "d" name = "question19"> d) 50 billion US<br>
+</div>
+
+<hr class="featurette-quizhr">
+
+<div class = "qheader">
+20) The first electronic computer was called?</div>
+<div class = "qselections">
+<input type = "radio" value = "a" name = "question20"> a) Univac<br>
+<input type = "radio" value = "b" name = "question20"> b) ENIAC<br>
+<input type = "radio" value = "c" name = "question20"> c) Apple lle<br>
+<input type = "radio" value = "d" name = "question20"> d) none of the above<br>
+</div>
+
+<hr class="featurette-divider">
+
+<div class="qheader">
+21)  Who was known as the Father of Computers?</div>
+<div class="qselections">
+<input type="radio" value="a" name="question21"> a)Blasie Pascal<br>
+<input type="radio" value="b" name="question21"> b)Joseph Jacquard<br>
+<input type="radio" value="c" name="question21"> c)Charles Babbage<br>
+<input type="radio" value="d" name="question21"> d)Herman Hollerith<br>
+</div>
+
+<hr class="featurette-quizhr">
+
+<div class="qheader">
+22)The Second Generation of computers used this to control operations:</div>
+<div class="qselections">
+<input type="radio" value="a" name="question22"> a)transistors<br>
+<input type="radio" value="b" name="question22"> b)microchips<br>
+<input type="radio" value="c" name="question22"> c)integrated circuits<br>
+<input type="radio" value="d" name="question22"> d)vacuum tubes<br>
+</div>
+
+<hr class="featurette-quizhr">
+
+<div class="qheader">
+23)Joseph Jacquard used to control he operations of his loom?</div>
+<div class="qselections">
+<input type="radio" value="a" name="question23"> a) Vacuum tubes<br>
+<input type="radio" value="b" name="question23"> b) electricity<br>
+<input type="radio" value="c" name="question23"> c) transistors<br>
+<input type="radio" value="d" name="question23"> d) punched cards<br>
+</div>
+
+<hr class="featurette-quizhr">
+
+<div class="qheader">
+24) The Fourth Generation of computers used this to control operations:</div>
+<div class="qselections">
+<input type="radio" value="a" name="question24"> a) integrated circuits<br>
+<input type="radio" value="b" name="question24"> b) transistors<br>
+<input type="radio" value="c" name="question24"> c) microchips<br>
+<input type="radio" value="d" name="question24"> d) vacuum tubes<br>
+</div>
+
+<hr class="featurette-quizhr">
+
+<div class="qheader">
+25) The arithmetic/logic unit of the computer:</div>
+<div class="qselections">
+<input type="radio" value="a" name="question25"> a) can add numbers<br>
+<input type="radio" value="b" name="question25"> b) can compare numbers<br>
+<input type="radio" value="c" name="question25"> c) is part of the central processing unit<br>
+<input type="radio" value="d" name="question25"> d) all of the above<br>
 </div>
 
 </form>
