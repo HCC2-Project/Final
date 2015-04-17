4b9bca5186a32f60166a551bd2171f45992aa359
x13508827 <x13508827@scr1-005.student.nci.col>
2015-04-17 13:59:53 +0100

    Quiz JavaScript modified

--- JS/ie-emulation-modes-warning.js
+++ JS/ie-emulation-modes-warning.js
@@ -54,9 +54,9 @@
 
 //JavaScript for quiz
 //Enter total number of questions:
-var totalquestions=10
+var totalquestions=25
 
-//Enter the solutions corresponding to each question:
+//the solutions corresponding to each question:
 var correctchoices=new Array()
 correctchoices[1]='c' //question 1 solution
 correctchoices[2]='a' //question 2 solution, and so on.
@@ -68,6 +68,21 @@ correctchoices[7]='b'
 correctchoices[8]='b'
 correctchoices[9]='c'
 correctchoices[10]='b'
+correctchoices[11]='a'
+correctchoices[12]='d' 
+correctchoices[13]='c'
+correctchoices[14]='c'
+correctchoices[15]='d'
+correctchoices[16]='c'
+correctchoices[17]='b'
+correctchoices[18]='b'
+correctchoices[19]='c'
+correctchoices[20]='b'
+correctchoices[21]='c' 
+correctchoices[22]='a' 
+correctchoices[23]='c'
+correctchoices[24]='c'
+correctchoices[25]='d'
 
 function gradeit(){
 var incorrect=null
@@ -95,31 +110,5 @@ else
 window.location="results1.php"
 }
 
-
-function showsolution(){
-var result=window.open("","result","width=200,height=350, scrollbars")
-result.focus()
-result.document.open()
-result.document.write('<title>Solution</title>')
-result.document.write('<body bgcolor="#FFFFCC">')
-result.document.write('<center><h3>Solution to Quiz</h3></center>')
-result.document.write('<center><font face="Arial">')
-for (i=1;i<=totalquestions;i++){
-for (temp=0;temp<incorrect.length;temp++){
-if (i==incorrect[temp])
-wrong=1
-}
-if (wrong==1){
-result.document.write("Question "+i+"="+correctchoices[i].fontcolor("red")+"<br>")
-wrong=0
-}
-else
-result.document.write("Question "+i+"="+correctchoices[i].fontcolor("green")+"<br>")
-}
-result.document.write('</center></font>')
-result.document.write("<h5>Note:The solutions in red are the ones to the questions you had incorrectly answered.</h5><p align='center'><small>")
-result.document.close()
-
-}
 
 //  End of javascript-->
