ebeaa0d01c229e992bd376239a5b811b07ba1aa2
kevgleeson78 <kevgleeson78@gmail.com>
2015-04-23 14:37:43 +0100

    Merged code

--- aboutus.php
+++ aboutus.php
@@ -6,73 +6,32 @@
 */
 require_once 'header.php';
 ?>
-<h2><span class="text-muted">First Generation (1940-1956) Vacuum Tubes</span></h2>
+<h2><span class="text-muted">Team KVMC</span></h2>
 <hr class="featurette-divider">
 <!-- START THE FEATURETTES -->
 <div class="container marketing">    
     
     <div class="row featurette">  
-        <p class="lead">Short History</p>
-        <img class="featurette-image img-responsive" src="Images/UNIVAC_computer.jpg" style="width: 400px; height: 300px;">
+        <p class="lead">About Us</p>
+        <img class="featurette-image img-responsive" src="Images/nci.jpg" style="width: 400px; height: 300px;">
 
         <p>  
-            The calculating engines of English mathematician Charles Babbage (1791-1871) are among the most celebrated icons in 
-            the prehistory of computing. Babbage’s Difference Engine No.1 was the first successful automatic calculator and 
-            remains one of the finest examples of precision engineering of the time. Babbage is sometimes referred to as 
-            "father of computing." The International Charles Babbage Society (later the Charles Babbage Institute) took his name 
-            to honor his intellectual contributions and their relation to modern computers.
-            Charles Babbage was born in London on December 26, 1791, the son of Benjamin Babbage, a London banker. As a youth 
-            Babbage was his own instructor in algebra, of which he was passionately fond, and was well read in the continental 
-            mathematics of his day. Upon entering Trinity College, Cambridge, in 1811, he found himself far in advance of his 
-            tutors in mathematics. Babbage co-founded the Analytical Society for promoting continental mathematics and reforming 
-            the mathematics of Newton then taught at the university. 
-            In his twenties Babbage worked as a mathematician, principally in the calculus of functions. He was elected a Fellow 
-            of the Royal Society in 1816 and played a prominent part in the foundation of the Astronomical Society (later Royal 
-            Astronomical Society) in 1820. It was about this time that Babbage first acquired the interest in calculating 
-            machinery that became his consuming passion for the remainder of his life.
-            The first computers used vacuum tubes for circuitry and magnetic drums for memory, and were often enormous, taking 
-            up entire rooms. Valve Computer Belongs to First Generation computers.
-            They were very expensive to operate and in addition to using a great deal of electricity, generated a lot of heat, 
-            which was often the cause of malfunctions.
-            First generation computers relied on machine language, the lowest-level programming language understood by computers, 
-            to perform operations, and they could only solve one problem at a time. 
-            Input was based on punched cards and paper tape, and output was displayed on printouts. The UNIVAC and ENIAC 
-            computers are examples of first-generation computing devices. 
-            The UNIVAC was the first commercial computer delivered to a business client, the U.S. Census Bureau in 1951.
+            Team KVMC are a group of web developer's  that have decided to come together and create a web based application's
+            on a Brief history of computing. At each major event along the timeline there will be more detailed information about the particular event that moved the technology forward.
+M-Learning or Mobile Learning is the concept of a user accessing a topic they are interested in, and learning more about the given topic as a result of the user interaction with the content of the application. Depending on the type of application the content can be accessed from MP3 players, notebooks, mobile phones and tablets. M-Learning allows for the user to access the content from anywhere as long as they have a mobile device. There is also the opportunity for users to share their experience with other users of the same application, aiding in a greater understanding of the source material.
+Gamification is a concept that awards the user in a way that increases interactivity with an application and records their progress. It can take the form of completing a profile, answering a questionnaire, answering quiz type questions and posting the results for other users to view. Awards are gained by the users in the form of new features being opened up within the application, higher profile status or a ranking place on a leaderboard. Gamification can be used as a powerful marketing tool, by the application administrators gathering and analysing the response of each user’s interaction within the application.
+In terms of E-Learning our application will aid people who have an interest in computers and would like to learn about the history of computing. It will also aid students who are thinking of entering a third level computer course.
+The gamification aspect of our application will be quizz based, with short multiple choice questions on each section of the timeline. The users results will be displayed on a leaderboard, allowing them to compare themselves with other users.
+
         </p>
-        <p>
-
-            Joseph-Marie Jacquard was not the inventor of the programmable loom, as many people imagine, actually he created an 
-            attachment to the loom, which played a very important role not only in the textile industry, but also in development 
-            of other programmable machines, such as computers, for example the Analytical Engine of Charles Babbage.
-
-            Joseph-Marie Charles (known as Jacquard after the nickname of his family) was born on 7th of July, 1752, in Lyon, 
-            France. He was the fifth of nine children of Jean Charles (nicknamed Jacquard) (1724-1772), a master weaver of 
-            brocaded fabrics, and his wife, Antoinette Rive. From the nine children, only Joseph and his sister Clémence 
-            (born 1747) survived to adulthood. Like the sons of many Lyons weavers, Joseph did not go to school, because his 
-            father needed him to perform odd jobs in the workshop. He learned to read as a thirteen-year-old boy by his 
-            brother-in-law Barret, a cultured man.
-
-            One of the first improvements of Jacquard was to eliminate the paper strip from Vaucanson s mechanism and to return 
-            to Falcons chain of punched cards. Then, he tried to avoid the expensive metal cylinders of Vaucanson. In fact, the.
-            term Jacquard loom is a misnomer, actually Jacquard s invented an attachment (head), that adapts to a great many type 
-            of looms, that allow the weaving machine to create the intricate patterns often seen in Jacquard weaving. Thus any 
-            loom that uses the attachment is called a Jacquard loom.
-
-            Each position in the punched card of the loom corresponds to a hook, which can either be raised or stopped dependant 
-            on whether the hole is punched out of the card or the card is solid. The hook raises or lowers the harness, which 
-            carries and guides the warp thread so that the weft will either lie above or below it. The sequence of raised and 
-            lowered threads is what creates the pattern. Each hook can be connected via the harness to a number of 
-            threads, allowing more than one repeat of a pattern. For example, a loom with a 500-hook head might have 
-            four threads connected to each hook, resulting in a fabric that is 2000 warp ends wide with four repeats of 
-            the weave going across. 
+       
 
         <hr class="featurette-divider">
 
         <!-- Three columns of text below the carousel -->
         <div class="row">
             <div class="col-lg-4">
-                <img class="img-circle" src="Images/cloud.jpg" style="width: 140px; height: 140px;">
+                <img class="img-circle" src="Images/nci.jpg" style="width: 140px; height: 140px;">
                 <h4><span class="text-muted">1st Computer Generation - Abhay Sharma</span></h4>
                 <!--<p>The development of electronic computers had clearly helped to visualize the concept of computer. The computer system...</p>-->
                 <p><a class="btn btn-default" href="http://histryofcomputr.blogspot.ie/2010/12/1st-gen.html" target="_blank" role="button">Read more... &raquo;</a></p>
