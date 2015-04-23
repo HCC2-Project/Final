e540112506519eda47000260c9a933b62d07fb6c
kevgleeson78 <kevgleeson78@gmail.com>
2015-04-23 14:26:05 +0100

    Merged code

--- aboutus.php
+++ aboutus.php
@@ -1,68 +1,110 @@
 <?php
-    
- require_once 'header.php';
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+require_once 'header.php';
+?>
+<h2><span class="text-muted">First Generation (1940-1956) Vacuum Tubes</span></h2>
+<hr class="featurette-divider">
+<!-- START THE FEATURETTES -->
+<div class="container marketing">    
+    
+    <div class="row featurette">  
+        <p class="lead">Short History</p>
+        <img class="featurette-image img-responsive" src="Images/UNIVAC_computer.jpg" style="width: 400px; height: 300px;">
 
-      echo' <div class="container marketing">
+        <p>  
+            The calculating engines of English mathematician Charles Babbage (1791-1871) are among the most celebrated icons in 
+            the prehistory of computing. Babbage’s Difference Engine No.1 was the first successful automatic calculator and 
+            remains one of the finest examples of precision engineering of the time. Babbage is sometimes referred to as 
+            "father of computing." The International Charles Babbage Society (later the Charles Babbage Institute) took his name 
+            to honor his intellectual contributions and their relation to modern computers.
+            Charles Babbage was born in London on December 26, 1791, the son of Benjamin Babbage, a London banker. As a youth 
+            Babbage was his own instructor in algebra, of which he was passionately fond, and was well read in the continental 
+            mathematics of his day. Upon entering Trinity College, Cambridge, in 1811, he found himself far in advance of his 
+            tutors in mathematics. Babbage co-founded the Analytical Society for promoting continental mathematics and reforming 
+            the mathematics of Newton then taught at the university. 
+            In his twenties Babbage worked as a mathematician, principally in the calculus of functions. He was elected a Fellow 
+            of the Royal Society in 1816 and played a prominent part in the foundation of the Astronomical Society (later Royal 
+            Astronomical Society) in 1820. It was about this time that Babbage first acquired the interest in calculating 
+            machinery that became his consuming passion for the remainder of his life.
+            The first computers used vacuum tubes for circuitry and magnetic drums for memory, and were often enormous, taking 
+            up entire rooms. Valve Computer Belongs to First Generation computers.
+            They were very expensive to operate and in addition to using a great deal of electricity, generated a lot of heat, 
+            which was often the cause of malfunctions.
+            First generation computers relied on machine language, the lowest-level programming language understood by computers, 
+            to perform operations, and they could only solve one problem at a time. 
+            Input was based on punched cards and paper tape, and output was displayed on printouts. The UNIVAC and ENIAC 
+            computers are examples of first-generation computing devices. 
+            The UNIVAC was the first commercial computer delivered to a business client, the U.S. Census Bureau in 1951.
+        </p>
+        <p>
+
+            Joseph-Marie Jacquard was not the inventor of the programmable loom, as many people imagine, actually he created an 
+            attachment to the loom, which played a very important role not only in the textile industry, but also in development 
+            of other programmable machines, such as computers, for example the Analytical Engine of Charles Babbage.
+
+            Joseph-Marie Charles (known as Jacquard after the nickname of his family) was born on 7th of July, 1752, in Lyon, 
+            France. He was the fifth of nine children of Jean Charles (nicknamed Jacquard) (1724-1772), a master weaver of 
+            brocaded fabrics, and his wife, Antoinette Rive. From the nine children, only Joseph and his sister Clémence 
+            (born 1747) survived to adulthood. Like the sons of many Lyons weavers, Joseph did not go to school, because his 
+            father needed him to perform odd jobs in the workshop. He learned to read as a thirteen-year-old boy by his 
+            brother-in-law Barret, a cultured man.
+
+            One of the first improvements of Jacquard was to eliminate the paper strip from Vaucanson s mechanism and to return 
+            to Falcons chain of punched cards. Then, he tried to avoid the expensive metal cylinders of Vaucanson. In fact, the.
+            term Jacquard loom is a misnomer, actually Jacquard s invented an attachment (head), that adapts to a great many type 
+            of looms, that allow the weaving machine to create the intricate patterns often seen in Jacquard weaving. Thus any 
+            loom that uses the attachment is called a Jacquard loom.
+
+            Each position in the punched card of the loom corresponds to a hook, which can either be raised or stopped dependant 
+            on whether the hole is punched out of the card or the card is solid. The hook raises or lowers the harness, which 
+            carries and guides the warp thread so that the weft will either lie above or below it. The sequence of raised and 
+            lowered threads is what creates the pattern. Each hook can be connected via the harness to a number of 
+            threads, allowing more than one repeat of a pattern. For example, a loom with a 500-hook head might have 
+            four threads connected to each hook, resulting in a fabric that is 2000 warp ends wide with four repeats of 
+            the weave going across. 
+
+        <hr class="featurette-divider">
+
+        <!-- Three columns of text below the carousel -->
+        <div class="row">
+            <div class="col-lg-4">
+                <img class="img-circle" src="Images/cloud.jpg" style="width: 140px; height: 140px;">
+                <h4><span class="text-muted">1st Computer Generation - Abhay Sharma</span></h4>
+                <!--<p>The development of electronic computers had clearly helped to visualize the concept of computer. The computer system...</p>-->
+                <p><a class="btn btn-default" href="http://histryofcomputr.blogspot.ie/2010/12/1st-gen.html" target="_blank" role="button">Read more... &raquo;</a></p>
+            </div><!-- /.col-lg-4 -->
+
+            <div class="col-lg-4">
+                <img class="img-circle" src="Images/cloud.jpg" style="width: 140px; height: 140px;">
+                <h4><span class="text-muted">1st Computer Generation - About Bendix G15</span></h4>
+                <!--<p>Why this interest in the Bendix G-15? Against the odds, the  Western Australian branch of The Australian Computer Museum Inc...</p>-->
+                <p><a class="btn btn-default" href="http://members.iinet.net.au/~dgreen/" target="_blank" role="button">Read more... &raquo;</a></p>
+            </div><!-- /.col-lg-4 -->
+
+            <div class="col-lg-4">
+                <img class="img-circle" src="Images/cloud.jpg" style="width: 140px; height: 140px;">
+                <h4><span class="text-muted">1st Computer Generation - John von Neumann</span></h4>
+                <!--<p>Von Neumann persuaded IAS to expand from doing theoretical studies to building a real computer...</p>-->
+                <p><a class="btn btn-default" href="http://www.computerhistory.org/revolution/birth-of-the-computer/4/92" target="_blank" role="button">Read more... &raquo;</a></p>
+            </div><!-- /.col-lg-4 -->
+        </div>
+        <hr class="featurette-divider">
+        <!-- /END THE FEATURETTES -->
+
+        <!-- FOOTER -->
+        <footer>
+            <p class="pull-right"><a href="#">Back to top</a></p>
+            <p>2015 Students-NCI, &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+        </footer>
+    </div>
+</div>
 
 
 
-            <!-- START THE FEATURETTES -->
 
-            <hr class="featurette-divider">
-
-            <div class="row featurette">
-                <div class="col-md-7">
-                    <h2 class="featurette-heading">First featurette heading. <span class="text-muted">Itll blow your mind.</span></h2>
-                    <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-                </div>
-                <div class="col-md-5">
-                    <img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-                </div>
-            </div>
-
-            <hr class="featurette-divider">
-
-            <div class="row featurette">
-                <div class="col-md-5">
-                    <img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-                </div>
-                <div class="col-md-7">
-                    <h2 class="featurette-heading">Oh yeah, its that good. <span class="text-muted">See for yourself.</span></h2>
-                    <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-                </div>
-            </div>
-
-            <hr class="featurette-divider">
-
-            <div class="row featurette">
-                <div class="col-md-7">
-                    <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-                    <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-                </div>
-                <div class="col-md-5">
-                    <img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-                </div>
-            </div>
-
-            <hr class="featurette-divider">
-
-            <!-- /END THE FEATURETTES -->
-
-
-            <!-- FOOTER -->
-            <footer>
-                <p class="pull-right"><a href="#">Back to top</a></p>
-                <p>2015 Students-NCI, &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
-            </footer>
-
-        </div><!-- /.container -->'
-              ?>
-
-    </span><br><br>
-  </body>
+</body>
 </html>
